20c059456fabedb48f4e7f691231b933624b97bf
rquesada <rquesadaqq@outlook.com>
2025-04-04 22:58:33 -0600

    Update the image app

--- image_converter/main.py
+++ image_converter/main.py
@@ -1,11 +1,3 @@
-import PIL
-
-def resize_image(image_path, output_path, width, height):
-    # Load the image
-    image = PIL.Image.open(image_path)
-    # Resize the image
-    resized_image = image.resize((width, height))
-    # Save the resized image
-    resized_image.save(output_path)
-    print(f"Image resized and saved to {output_path}")
-    return resized_image
+from PIL import Image
+im = Image.open("bride.jpg")
+im.rotate(45).show()
