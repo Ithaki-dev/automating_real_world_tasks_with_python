211881f1c2b808e50b1b4814bcc8a0fdb592ccac
rquesada <rquesadaqq@outlook.com>
2025-04-05 01:43:02 -0600

    Upgrade using Pillow module

--- image_converter/main.py
+++ image_converter/main.py
@@ -1,3 +1,8 @@
 from PIL import Image
-im = Image.open("bride.jpg")
+import os
+# Current phat to the image file
+current_path = os.path.dirname(__file__)
+image_path = os.path.join(current_path, "bride.jpg")
+# Open the image file and rotate it by 45 degrees
+im = Image.open(image_path)
 im.rotate(45).show()
