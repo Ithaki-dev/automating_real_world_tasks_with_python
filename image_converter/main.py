38f66ac3335d3c0d09351a1c1bfcbd5f58030e5d
rquesada <rquesadaqq@outlook.com>
2025-04-05 19:29:33 -0600

    Play with PIL functions and generate a new image

--- image_converter/main.py
+++ image_converter/main.py
@@ -3,6 +3,12 @@ import os
 # Current phat to the image file
 current_path = os.path.dirname(__file__)
 image_path = os.path.join(current_path, "bride.jpg")
+new_image_path = os.path.join(current_path, "new_bride.jpg")
 # Open the image file and rotate it by 45 degrees
 im = Image.open(image_path)
-im.rotate(45).show()
+# Rotate the image by 45 degrees
+# im.rotate(45).show()
+# Resize the image to 50% of its original size
+# im = im.resize((400,600)).show()
+# Save the image in PNG format
+new_img = im.resize((900,1000)).rotate(180).save(new_image_path)
